824aae500fe9a63e2f2e40ea63276c03a18faadb
jangwonseok <jangws1003@naver.com>
2025-03-09 22:14:23 +0900

    플러터 테스트중...

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class ToDoApp extends StatefulWidget {
 
 class _ToDoAppState extends State<ToDoApp> {
   List<String> todos = ["플러터 공부하기", "운동하기", "코드 리뷰하기"];
+  List<bool> isCompleted = [false, false, false]; // 완료 여부 저장
   TextEditingController _controller = TextEditingController();
 
   // 할 일 수정 다이얼로그
@@ -59,6 +60,13 @@ class _ToDoAppState extends State<ToDoApp> {
     );
   }
 
+  // 체크박스 상태 변경
+  void _toggleComplete(int index) {
+    setState(() {
+      isCompleted[index] = !isCompleted[index];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,7 +75,19 @@ class _ToDoAppState extends State<ToDoApp> {
         itemCount: todos.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(todos[index]),
+            leading: Checkbox(
+              value: isCompleted[index],
+              onChanged: (value) => _toggleComplete(index), // 체크하면 상태 변경
+            ),
+            title: Text(
+              todos[index],
+              style: TextStyle(
+                decoration: isCompleted[index]
+                    ? TextDecoration.lineThrough // 완료 시 취소선 추가
+                    : TextDecoration.none,
+                color: isCompleted[index] ? Colors.grey : Colors.black, // 완료 시 회색으로 변경
+              ),
+            ),
             onTap: () => _editTask(index), // 클릭하면 수정 다이얼로그 실행
           );
         },
